d67dc01dd81ba1662ed389eddf6f7f68726618d2
Diane Dushimimana <dushimediane12@gmail.com>
2024-10-21 13:46:37 +0200

    all

--- src/View/home.js
+++ src/View/home.js
@@ -104,104 +104,133 @@ const Home = () => {
       </section>
 
       <section class="service-details">
-
-      <div class="container py-5">
-    <h5 class="text-center mb-4"><a href="#" class="text-decoration-none text-dark">Our Key Activities</a></h5>
-
+  <div class="container py-5">
+    <h5 class="text-center mb-4">
+      <a href="#" class="text-decoration-none text-dark">Our Key Activities</a>
+    </h5>
     <div class="row g-4">
-       
-        <div class="col-md-6" data-aos="fade-up">
-            <div class="card h-100 shadow-sm">
-                <div class="card-img">
-                    <img src="assets/images/2.jpg" class="card-img-top" alt="Education Support Activities"/>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title"><a href="#" class="text-decoration-none">Education Support Activities</a></h5>
-                    <p class="card-text">Gama Global Network provides comprehensive educational support, offering courses in English, French, German, and Kiswahili, along with training in videography, photography, editing, filming, animation, and graphic design...</p>
-                    <div class="read-more"><a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a></div>
-                </div>
+      <div class="col-md-6 col-lg-4" data-aos="fade-up">
+        <div class="card h-100 shadow-sm">
+          <div class="card-img">
+            <img src="assets/images/2.jpg" class="card-img-top img-fluid" alt="Education Support Activities" />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">
+              <a href="#" class="text-decoration-none">Education Support Activities</a>
+            </h5>
+            <p class="card-text">
+              Gama Global Network provides comprehensive educational support, offering courses in English, French, German, and Kiswahili, along with training in videography, photography, editing, filming, animation, and graphic design...
+            </p>
+            <div class="read-more">
+              <a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a>
             </div>
+          </div>
         </div>
-        
+      </div>
 
-        <div class="col-md-6" data-aos="fade-up">
-            <div class="card h-100 shadow-sm">
-                <div class="card-img">
-                    <img src="assets/images/3.avif" class="card-img-top" alt="Leveraging ICT for Empowerment"/>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title"><a href="#" class="text-decoration-none">Leveraging ICT for Empowerment</a></h5>
-                    <p class="card-text">Initiate and leverage Information and Communication Technology (ICT) to empower individuals, fostering a unique sense of community well-being through innovative digital initiatives.</p>
-                    <div class="read-more"><a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a></div>
-                </div>
+      <div class="col-md-6 col-lg-4" data-aos="fade-up">
+        <div class="card h-100 shadow-sm">
+          <div class="card-img">
+            <img src="assets/images/3.avif" class="card-img-top img-fluid" alt="Leveraging ICT for Empowerment" />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">
+              <a href="#" class="text-decoration-none">Leveraging ICT for Empowerment</a>
+            </h5>
+            <p class="card-text">
+              Initiate and leverage Information and Communication Technology (ICT) to empower individuals, fostering a unique sense of community well-being through innovative digital initiatives.
+            </p>
+            <div class="read-more">
+              <a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a>
             </div>
+          </div>
         </div>
+      </div>
 
-
-        <div class="col-md-6" data-aos="fade-up">
-            <div class="card h-100 shadow-sm">
-                <div class="card-img">
-                    <img src="assets/images/4.jpg" class="card-img-top" alt="Promoting Sustainable Agriculture Practices"/>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title"><a href="#" class="text-decoration-none">Promoting Sustainable Agriculture Practices</a></h5>
-                    <p class="card-text">Undertake projects that promote sustainable agricultural practices, contributing to both environmental consciousness and community development in a distinctive way.</p>
-                    <div class="read-more"><a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a></div>
-                </div>
+      <div class="col-md-6 col-lg-4" data-aos="fade-up">
+        <div class="card h-100 shadow-sm">
+          <div class="card-img">
+            <img src="assets/images/4.jpg" class="card-img-top img-fluid" alt="Promoting Sustainable Agriculture Practices" />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">
+              <a href="#" class="text-decoration-none">Promoting Sustainable Agriculture Practices</a>
+            </h5>
+            <p class="card-text">
+              Undertake projects that promote sustainable agricultural practices, contributing to both environmental consciousness and community development in a distinctive way.
+            </p>
+            <div class="read-more">
+              <a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a>
             </div>
+          </div>
         </div>
+      </div>
 
-        
-        <div class="col-md-6" data-aos="fade-up">
-            <div class="card h-100 shadow-sm">
-                <div class="card-img">
-                    <img src="assets/images/6.png" class="card-img-top" alt="Financial Literacy Initiatives"/>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title"><a href="#" class="text-decoration-none">Financial Literacy and Responsible Banking Initiatives</a></h5>
-                    <p class="card-text">Implement initiatives related to financial literacy and responsible banking, cultivating a unique atmosphere of financial responsibility within the community.</p>
-                    <div class="read-more"><a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a></div>
-                </div>
+      <div class="col-md-6 col-lg-4" data-aos="fade-up">
+        <div class="card h-100 shadow-sm">
+          <div class="card-img">
+            <img src="assets/images/6.png" class="card-img-top img-fluid" alt="Financial Literacy Initiatives" />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">
+              <a href="#" class="text-decoration-none">Financial Literacy and Responsible Banking Initiatives</a>
+            </h5>
+            <p class="card-text">
+              Implement initiatives related to financial literacy and responsible banking, cultivating a unique atmosphere of financial responsibility within the community.
+            </p>
+            <div class="read-more">
+              <a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a>
             </div>
+          </div>
         </div>
+      </div>
 
-        
-        <div class="col-md-6" data-aos="fade-up">
-            <div class="card h-100 shadow-sm">
-                <div class="card-img">
-                    <img src="assets/images/9.jpg" class="card-img-top" alt="Substance Abuse and Rehabilitation"/>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title"><a href="#" class="text-decoration-none">Addressing Substance Abuse and Rehabilitation</a></h5>
-                    <p class="card-text">Develop and execute projects aimed at addressing substance abuse and supporting rehabilitation efforts, emphasizing a unique and comprehensive approach to recovery.</p>
-                    <div class="read-more"><a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a></div>
-                </div>
+      <div class="col-md-6 col-lg-4" data-aos="fade-up">
+        <div class="card h-100 shadow-sm">
+          <div class="card-img">
+            <img src="assets/images/9.jpg" class="card-img-top img-fluid" alt="Substance Abuse and Rehabilitation" />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">
+              <a href="#" class="text-decoration-none">Addressing Substance Abuse and Rehabilitation</a>
+            </h5>
+            <p class="card-text">
+              Develop and execute projects aimed at addressing substance abuse and supporting rehabilitation efforts, emphasizing a unique and comprehensive approach to recovery.
+            </p>
+            <div class="read-more">
+              <a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a>
             </div>
+          </div>
         </div>
+      </div>
 
-        
-        <div class="col-md-6" data-aos="fade-up">
-            <div class="card h-100 shadow-sm">
-                <div class="card-img">
-                    <img src="assets/images/courses-04.jpg" class="card-img-top" alt="Health and Well-being Initiatives"/>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title"><a href="#" class="text-decoration-none">Health and Well-being Initiatives</a></h5>
-                    <p class="card-text">Launch initiatives that promote overall health and well-being, offering a distinctive perspective on community health programs.</p>
-                    <div class="read-more"><a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a></div>
-                </div>
+      <div class="col-md-6 col-lg-4" data-aos="fade-up">
+        <div class="card h-100 shadow-sm">
+          <div class="card-img">
+            <img src="assets/images/courses-04.jpg" class="card-img-top img-fluid" alt="Health and Well-being Initiatives" />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">
+              <a href="#" class="text-decoration-none">Health and Well-being Initiatives</a>
+            </h5>
+            <p class="card-text">
+              Launch initiatives that promote overall health and well-being, offering a distinctive perspective on community health programs.
+            </p>
+            <div class="read-more">
+              <a href="#" class="text-primary"><i class="bi bi-arrow-right"></i> Read More</a>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-
-      </section>
+  </div>
+</section>
       {/* Footer Section */}
       <footer>
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <p><i className="fa fa-copyright"></i> Copyright 2024 by Gama Grobal Network
+              <p><i className="fa fa-copyright"></i> Copyright 2024 by Gama Global Network
 
 
 
